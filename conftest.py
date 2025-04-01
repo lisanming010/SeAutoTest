e2fa6622a1e809c2cb05d340d8ac5932e534d638
lisanming_laptop <lsm_lil@outlook.com>
2025-04-01 18:49:44 +0800

    TODO:pytest初始化webdriver并传递至用例

--- conftest.py
+++ conftest.py
@@ -13,16 +13,25 @@ def init_logger():
     return logger
 
 @pytest.fixture(scope="session", autouse=True)
-def init_driver_and_longin():
+def init_driver():
     edge_options = webdriver.EdgeOptions()
 
     edge_options.add_argument('--ignore-certificate-errors')  #忽略ssl证书错误
     edge_options.add_argument('--ignore-ssl-errors')
     edge_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     driver = webdriver.Edge(options=edge_options)
+    driver.implicitly_wait(10)
     logging.info("driver 初始化成功！")
 
+    yield driver
+
+    driver.close()
+
+@pytest.fixture()
+def login(request):
+    driver = init_driver()
     logger = init_logger()
+
     driver.get('https://10.16.204.131')
     elements_selector = LoginPageEles(driver, logger)
     username_input = elements_selector.user_name_input()
@@ -30,5 +39,4 @@ def init_driver_and_longin():
     login_button = elements_selector.login_button()
     ActionChains(driver).click(username_input).send_keys('admin').click(passwd_input).send_keys('Pass@admin2024').click(login_button).perform()
     sleep(10)
-    yield
-    driver.close()
+    return driver, logger
--- test_cases/vm_test/test_vm_create.py
+++ test_cases/vm_test/test_vm_create.py
@@ -10,8 +10,10 @@ from time import sleep
 #     login_button = elements_selector.login_button()
 #     ActionChains(driver).click(username_input).send_keys('admin').click(passwd_input).send_keys('Pass@admin2024').click(login_button).perform()
 
-def test_run(driver, logger):
+
+def test_run(login):
     # run_login(driver, logger)
+    driver, logger = login
     elements_selector = InstanceList(driver, logger)
     comput_button = elements_selector.compute_button()
     ActionChains(driver).click(comput_button).perform()
