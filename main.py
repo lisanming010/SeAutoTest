b07abbdee11dd15ac256ce5f38710d7251db8cbb
lisanming_laptop <lsm_lil@outlook.com>
2025-04-03 11:40:30 +0800

    优化

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import requests
-import warnings
 import pytest
 from selenium import webdriver
-from multiprocessing import Process, Pool
-from utils.log_handle.log_handle import LoggerSetUp
 from utils.confing_handle import HandleConfig
-from test_cases.login_test import test_login
-from test_cases.vm_test import test_vm_create
 from requests.packages import urllib3
 
 SETTIGN_FILE = 'setting.ini'
