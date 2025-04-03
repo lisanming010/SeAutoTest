5649fbec06d7dc738142c9a6aef59bd58b66ae25
lisanming_laptop <lsm_lil@outlook.com>
2025-04-03 11:12:02 +0800

    Handler_conf 优化

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ from test_cases.login_test import test_login
 from test_cases.vm_test import test_vm_create
 from requests.packages import urllib3
 
-def init_conf_hadler():
-    conf_handler = HandleConfig('setting.ini')
-    return conf_handler
+SETTIGN_FILE = 'setting.ini'
 
 def make_pytest_conf():
-    conf_hadler = init_conf_hadler()
-    conf_hadler.conf_write()
+    pytest_conf = HandleConfig(SETTIGN_FILE).read_section('pytest')
+    HandleConfig('pytest.ini').conf_write('pytest', pytest_conf)
+
+def update_pytest_conf():
+    run_nums = HandleConfig(SETTIGN_FILE).get_value_str('global', 'number_of_process')
+    pytest_addopts = HandleConfig(SETTIGN_FILE).get_value_str('pytest', 'addopts')
+    value = pytest_addopts + f' -n {run_nums}'
+    options = [('addopts', value)]
+    HandleConfig('pytest.ini').update_conf('pytest', options)
 
 if __name__ == '__main__':
     make_pytest_conf()
+    update_pytest_conf()
     pytest.main()
--- utils/confing_handle.py
+++ utils/confing_handle.py
@@ -4,12 +4,10 @@ import os
 
 class HandleConfig:
     def __init__(self, filename):
+        self.filename = filename
+        self.conf = configparser.RawConfigParser()
         if os.path.exists(filename):
-            self.filename = filename
-            self.conf = configparser.RawConfigParser()
             self.conf.read(self.filename, encoding='UTF-8')
-        else:
-            raise FileNotFoundError('文件不存在！')
 
     def read_section(self, section):
         return self.conf.items(section)
@@ -26,21 +24,25 @@ class HandleConfig:
     def get_eval_data(self, section, option):
         return ast.literal_eval(self.conf.get(section, option))
     
-    def conf_write(self, conf_file_name='pytest.ini', section='pytest'):
-        """
-        从setting中提取pytest section生成新pytest.ini配置文件
-
-        : conf_file_name 新配置文件名称
-        : section setting文件中section区域
-        """
+    def conf_write(self, section, options):
         
-        conf_new= configparser.RawConfigParser()
-        optins = self.read_section(section)
-        conf_new.add_section(section)
-        for option, value in optins:
-            conf_new.set(section, option, value)
+        if self.conf.has_section(section):
+            self.conf.remove_section(section)
+        self.conf.add_section(section)
+        for option, value in options:
+            self.conf.set(section, option, value)
 
-        if os.path.exists(conf_file_name):
-            os.remove(conf_file_name)
-        with open(conf_file_name, 'x') as f:
-            conf_new.write(f)
+        with open(self.filename, 'w') as f:
+            self.conf.write(f)
+
+    def update_conf(self, section, options):
+        
+        if not self.conf.has_section(section):
+            self.conf.add_section(section)
+        for option, value in options:
+            if self.conf.has_option(section, option):
+                self.conf.remove_option(section, option)
+            self.conf.set(section, option, value)
+
+        with open(self.filename, 'w') as f:
+            self.conf.write(f)
